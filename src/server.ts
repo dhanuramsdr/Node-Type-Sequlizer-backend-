39d60a617973c86ae7e7fb3d0f6a860a886c5d9b
dhanuramsdr <dhanuramseenivasan@gmail.com>
2026-03-02 11:23:22 -0500

    changes updated

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())  
 app.use(express.urlencoded({ extended: true })) 
 
-// 3. Routes AFTER body parsers
+
 app.use("/api/v1", allRouter)
 
 const startServer = async () => {
