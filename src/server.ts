f0653f5adda8075266c382a01b9be56214282af0
dhanuramsdr <153482055+dhanuramsdr@users.noreply.github.com>
2026-03-03 22:00:08 +0530

    Add comment to clarify app.use functionality
    Added a comment explaining the use of app.use.

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())  
 app.use(express.urlencoded({ extended: true })) 
 
-//app.use
+//app.use is used 
 app.use("/api/v1", allRouter)
 
 const startServer = async () => {
